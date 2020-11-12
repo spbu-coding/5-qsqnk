58749b59a84df4a22ba1fdfae7584ef4b6ed883e
qsqnk <71890589+qsqnk@users.noreply.github.com>
2020-11-12 19:40:19 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,8 +2,16 @@
 #include <string.h>
 
 void input() {
+	int a = 0;
     char buffer[5];
+    memcpy(&a, buffer + 17, 4);
+    printf("%x\n", a);
+    fflush(stdout);
     scanf("%s", buffer);
+    memcpy(&a, buffer + 17, 4);
+    printf("%x\n", a);
+    puts(buffer);
+    fflush(stdout);
 }
 
 void other() {
